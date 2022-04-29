b891901829702cd5d6f1633a9d153bf0936ac309
Jon Wyatt <>
2022-04-29 09:43:39 +0100

    Rename to /case-v3; open contingency content by default

--- app/routes.js
+++ app/routes.js
@@ -3,14 +3,14 @@ const router = express.Router()
 const caseResponse = require('./data/get-case-overview.json')
 
 // Add your routes here - above the module.exports line
-router.get('/case-v2/:sectionId', (req, res, next) => {
+router.get('/case-v3/:sectionId', (req, res, next) => {
   res.locals.case = caseResponse
-  res.locals.pageUrlBase = '/case-v2/'
+  res.locals.pageUrlBase = '/case-v3/'
   res.locals.section = {
     id: req.params.sectionId
   }
   res.locals.compact = Boolean(typeof req.query.compact !== 'undefined')
-  res.render('case-v2/index.html')
+  res.render('case-v3/index.html')
 })
 
 module.exports = router
