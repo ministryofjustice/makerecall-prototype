a07e3dbd519e56842ed952dc4d686899c2caa33e
Paul O'Neill <paul.oneill@digital.justice.gov.uk>
2022-09-21 02:34:11 +0100

    home page updated

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Deciding if someone should be recalled or not',
 
   // Default port that prototype runs on
   port: '3000',
